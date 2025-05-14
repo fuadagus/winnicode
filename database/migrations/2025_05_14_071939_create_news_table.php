626faefd77b8c7b1e21e7fc4a846ea420b2e068b
fuadagus <fuadagussalim@ugm.ac.id>
2025-05-14 14:20:28 +0700

    make news table migration

---
+++ database/migrations/2025_05_14_071939_create_news_table.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image_url')->nullable();
+            $table->foreignId('location_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+}
